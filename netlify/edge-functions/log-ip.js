e4d33837b40779ef5e3ee147405a32220d65922a
niteoowl <xx.niteowl@gmail.com>
2025-06-28 15:17:10 +0900

    Update log-ip.js

--- netlify/edge-functions/log-ip.js
+++ netlify/edge-functions/log-ip.js
@@ -6,6 +6,10 @@ export default async (request, context) => {
     request.headers.get('client-ip') ||
     '알 수 없음';
 
+  // --- 추가된 부분: IP 주소를 Netlify Edge Function 로그에 기록합니다. ---
+  context.log(`Edge Function IP Log: ${ip}`);
+  // ------------------------------------------------------------------
+
   return new Response(`Your IP is: ${ip}`, {
     headers: {
       'Content-Type': 'text/plain'
