4ec963d3c33c1967f479899cd5eb08002bee5e0b
niteoowl <xx.niteowl@gmail.com>
2025-06-28 10:11:18 +0900

    Update log-ip.js

--- netlify/edge-functions/log-ip.js
+++ netlify/edge-functions/log-ip.js
@@ -2,10 +2,13 @@ export default async (request, context) => {
   const ip =
     request.headers.get('x-nf-client-connection-ip') ||
     request.headers.get('x-forwarded-for') ||
+    request.headers.get('cf-connecting-ip') ||
     request.headers.get('client-ip') ||
     '알 수 없음';
 
-  return new Response(`🔍 Your IP is: ${ip}`, {
-    headers: { 'Content-Type': 'text/plain' }
+  return new Response(`Your IP is: ${ip}`, {
+    headers: {
+      'Content-Type': 'text/plain'
+    }
   });
 };
